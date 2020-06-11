7c2ba87bae2697a7b65ac94721e35c6bbeffaf82
Ninger-Gong <61758760+Ninger-Gong@users.noreply.github.com>
2020-06-12 10:59:00 +1200

    Files needed to be test

---
+++ DetailViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.shopapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_view);
+    }
+}
